d97ada98727f581390a04d12958c01149c815af0
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-11-22 02:51:21 +0000

    Initial commit

---
+++ src/Heading.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My favorite foods</h1>;
+}
+export default Heading;
---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Paneer</li>
+      <li>Mushroom</li>
+      <li>Mithai</li>
+    </ul>
+  );
+}
+
+export default List;
